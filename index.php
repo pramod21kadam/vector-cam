6814e967eb4aee106c4b5807ac2dde7c06ecc7df
Onkar Kunjir <onkarkunjir8@gmail.com>
2020-03-15 11:47:45 +0530

    adding animations to login page and navbar.

--- index.php
+++ index.php
@@ -1,32 +1,38 @@
 <?php
     session_start();
 ?>
-<!DOCTYPE html>
 
+<!DOCTYPE html>
 <html>
     <head>
         <title>VectorCam</title>
+        <link rel="stylesheet" href="static/bootstrap/css/bootstrap.min.css">
     </head>
 
     <body>
-        <header>
-            <ul>
-                <li><a href="static/shop.html">Shop</a></li>
-                <li><a href="static/about.html">About</a></li>
-                <li>
-                    <?php 
-                        if($_SESSION["mail_id"]){
-                            echo('<a href="#">My Account</a>');
-                        }
-                        else{
-                            echo('<a href="static/login.html">Login</a>');
-                        }
-                    ?>
-                </li>
-            </ul>
-        </header>
+    <nav class="navbar sticky-top navbar-light bg-light">
+        <a class="navbar-brand justify-content-start" href="#">VectorCam</a>
 
-        
+        <ul class="nav justify-content-end">
+            <li class="nav-item">
+                <a class="nav-link active" href="static/shop.html">Shop</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="static/about.html">About</a>
+            </li>
+            <li class="nav-item">
+                <?php 
+                    if($_SESSION["mail_id"]){
+                        echo('<a href="#" class="nav-link">My Account</a>');
+                    }
+                    else{
+                        echo('<a href="static/login.html" class="nav-link">Login</a>');
+                    }
+                ?>
+            </li>
+        </ul>
+    </nav>
+
     </body>
 
 </html>
