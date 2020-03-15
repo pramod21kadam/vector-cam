27a74f2f391ea8f10d14be86fe47693d22bd85e6
Onkar Kunjir <onkarkunjir8@gmail.com>
2020-03-15 14:22:27 +0530

    organizing code.

--- db_handler.php
+++ db_handler.php
@@ -1,10 +1,8 @@
 <?php
    class db_handler{
-
        public function sign_up($email_id , $phone_number , $password){
            return true;
        }
-
        public function sign_in($email_id , $password){
            return true;
        }
--- index.php
+++ index.php
@@ -10,29 +10,8 @@
     </head>
 
     <body>
-    <nav class="navbar sticky-top navbar-light bg-light">
-        <a class="navbar-brand justify-content-start" href="#">VectorCam</a>
-
-        <ul class="nav justify-content-end">
-            <li class="nav-item">
-                <a class="nav-link active" href="static/shop.html">Shop</a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="static/about.html">About</a>
-            </li>
-            <li class="nav-item">
-                <?php 
-                    if($_SESSION["mail_id"]){
-                        echo('<a href="#" class="nav-link">My Account</a>');
-                    }
-                    else{
-                        echo('<a href="static/login.html" class="nav-link">Login</a>');
-                    }
-                ?>
-            </li>
-        </ul>
-    </nav>
-
+        <?php require"nav_bar.php" ?>
+        <p>home</p>
     </body>
 
 </html>
