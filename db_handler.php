023e8cc2900920e03b50a674cc1485f201111b7c
pramod21kadam <pramod21dkadam@gmail.com>
2020-03-16 16:04:21 +0530

    login and signup functions

--- db_handler.php
+++ db_handler.php
@@ -1,14 +1,37 @@
 <?php
    class db_handler{
-       public function sign_up($f_name , $l_name , $email_id , $password){
-           return true;
-       }
-       public function sign_in($email_id , $password){
-           return true;
-       }
+        public $conn;
 
-       public function delete_account($email , $password){
+        function __construct() {
+            $servername = "localhost";
+            $username = "username";
+            $password = "localhost";
+
+            $this->conn = mysqli_connect($servername, $username, $password);
+        }
+
+        public function sign_up($f_name , $l_name , $email_id , $password){
+            $sql = "INSERT INTO customer(email,password,first_name,last_name) values('". $email_id ."','". $password ."','". $f_name ."','".$l_name."');";
+            if ( $conn->query($sql) === TRUE) {
+                return TRUE;
+            } else {
+                echo "Error: " . $sql . "<br>" . $conn->error;
+                return FALSE;
+            }
+
+        }
+        
+        public function sign_in($email_id , $password){
+            $sql = "select count(*) from customer where email ='". $email_id ."' password = '". $password ."';";
+            $result = $conn->query(sql);
+            if ($result == 0){
+                return false;
+            }
+            return true;
+        }
+
+        public function delete_account($email , $password){
            return true;
-       }
-   }
+        }
+    }
 ?>
