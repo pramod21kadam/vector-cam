cf245d4663ba7ad0e4dbd2856095c4c3e67a0c8c
pramod21kadam <pramod21dkadam@gmail.com>
2020-03-18 22:03:27 +0530

    don't change my  code

--- db_handler.php
+++ db_handler.php
@@ -1,34 +1,37 @@
 <?php
    class db_handler{
         var $conn;
-
         //Constructor
         function __construct() {
             $servername = "localhost";
-            $username = "onkar";
-            $password = "onkar123";
+            $username = "username";
+            $password = "password";
             $db = 'vector_cam';
             $this->conn = new mysqli( $servername, $username, $password, $db );
         }
 
         //Signup function
+        //function will not execute if parameters passed are null 
         public function sign_up($f_name , $l_name , $email_id , $password){
-            $sql = "insert into customer(password , email,first_name,last_name) values('".$password."','".$email_id."','".$f_name."','".$l_name."');";
-            if ( ($this->conn->query($sql)) == true) {
-                return true;
-            } else {
-                return false;
+            if(true){
+                $sql = "insert INTO customer(email,password,first_name,last_name) values('$email_id','$password','$f_name ',' $l_name');";
+                if ( ($this->conn->query($sql)) == TRUE) {
+                    return TRUE;
+                } else {
+                    echo "Error: " . $sql . "<br>" . $conn->error;
+                    return FALSE;
+                }
             }
         }
 
         //Signin function
         public function sign_in($email_id , $password){
-            $sql = "select * from customer where email ='".$email_id."' and password = '".$password."';";
-            $result = $this->conn->query($sql);
-            if ($result->num_rows > 0){
-                return true;
+            $sql = "SELECT count(*) from customer where email = '$email_id' password = '$password';";
+            $result = mysqli_fetch_row($this->conn->query(sql));
+            if ($result){
+                return false;
             }
-            return false;
+            return true;
         }
 
 
@@ -52,7 +55,25 @@
 
         // function deletes cart
         public function delete_cart(){
-            
+            $email = _SESSION["mail_id"];
+            $sql = "select cart_id from cart where email='$email'";
+            $cart_id = mysqli_fetch_row($this->conn->query($sql));
+            //take elements from cart_item and back to product table
+                    $sql = "SELECT product_id,quantity FROM cart_item where cart_id='$cart_id[0]'";
+                    $product_details = ($this->conn->query($sql));
+                    
+                    while( $row = $product_details -> fetch_row()){
+                        $product_id = $row[0];
+                        $_quantity = row[1];
+                        //take quantity of product from product table
+                        $sql = "SELECT quantity FROM product WHERE product_id ='$product_id';";
+                        $product_quantity = (($this->conn->query($sql))->fetch_row())[0];
+                        $product_quantity += $_quantity;
+                    }            
+            //clear cart item table
+            //clear entery from payment table
+            //clear payment field from customer table
+            //clear cart table
         }
 
         // function returns cart details
@@ -72,18 +93,13 @@
 
         // private functions
         private function check_cart(){
-            $email = session_id();
+            $email = _SESSION["mail_id"];
             $cart_id = mysqli_fetch_row($this->conn->query("SELECT cart_id FROM cart WHERE email='$email';"));
             if ($cart_id == null){
                 return false;
             }else{
             return $cart_id;
             }
-        }
-
-
-        function __destruct(){
-            $this->conn->close();
         }
     }
 ?>
